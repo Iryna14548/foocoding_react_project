b2d7fd61ac4a10d6fafc11433eee92ee9c7643e0
Iryna <ira.14548@gmail.com>
2024-05-17 15:09:28 +0200

    CartBookItem + CartPage

---
+++ src/components/Cart/CartBookItem.tsx
@@ -0,0 +1,94 @@
+import { useContext, useMemo } from 'react';
+import { BookCart } from '../Books/interfacesBook';
+import { CartContext } from '../context/CartContext/CartContext';
+import './CartBookItem.css';
+
+interface CartBookItemProps {
+    book: BookCart;
+}
+
+export default function CartBookItem({ book }: CartBookItemProps) {
+    const { cartBooks, setCartBooks } = useContext(CartContext);
+
+    const removeFromCart = (bookToDelete: BookCart) => {
+        setCartBooks(cartBooks.filter((book) => book.id !== bookToDelete.id));
+    };
+
+    const addQuantity = (bookToAdd: BookCart) => {
+        setCartBooks(
+            cartBooks.map((book) => {
+                if (book.id === bookToAdd.id) {
+                    return {
+                        ...book,
+
+                        quantity: book.quantity < 10 ? book.quantity + 1 : book.quantity,
+                    };
+                }
+                return book;
+            })
+        );
+    };
+
+    const removeQuantity = (bookToRemove: BookCart) => {
+        setCartBooks(
+            cartBooks.map((book) => {
+                if (book.id === bookToRemove.id) {
+                    return {
+                        ...book,
+                        quantity: book.quantity > 1 ? book.quantity - 1 : 1,
+                    };
+                }
+                return book;
+            })
+        );
+    };
+
+    const bookPrice = useMemo(() => {
+        return book.price * book.quantity;
+    }, [book.price, book.quantity]);
+
+    return (
+        <li className="cart-list-item">
+            <div className="cart-list-item__image-wrapper">
+                <img src={book.cover} alt={book.title} className="cart-list-item__image" />
+            </div>
+            <div className="cart-list-item__details-wrapper">
+                <div className="cart-list-item__name-wrapper">
+                    <h3 className="cart-list-item__name">{book.title}</h3>
+
+                    <span className="cart-list-item__quantity">Quantity</span>
+                    <button className="cart-list-item__button">
+                        <button
+                            className="cart-list-item__button-count"
+                            onClick={() => {
+                                removeQuantity(book);
+                            }}
+                        >
+                            -
+                        </button>{' '}
+                        {book.quantity}{' '}
+                        <button
+                            className="cart-list-item__button-count"
+                            onClick={() => {
+                                addQuantity(book);
+                            }}
+                        >
+                            +
+                        </button>
+                    </button>
+                </div>
+                <div className="cart-list-item__price-wrapper">
+                    <p className="cart-list-item__price">{bookPrice} kr SEK</p>
+                    <button
+                        className="cart-list-item__remove-button"
+                        onClick={() => {
+                            removeFromCart(book);
+                        }}
+                    >
+                        remove
+                    </button>
+                </div>
+            </div>
+        </li>
+    );
+}
--- src/components/Pages/CartPage.tsx
+++ src/components/Pages/CartPage.tsx
@@ -2,43 +2,10 @@ import { useContext } from 'react';
 import './CartPage.css';
 
 import { CartContext } from '../context/CartContext/CartContext';
-import { Book } from '../Books/interfacesBook';
+import CartBookItem from '../Cart/CartBookItem';
 
 export default function CartPage() {
-    const { cartBooks, setCartBooks } = useContext(CartContext);
-
-    const removeFromCart = (bookToDelete: Book) => {
-        setCartBooks(cartBooks.filter((book) => book.id !== bookToDelete.id));
-    };
-
-    const addQuantity = (bookToAdd: Book) => {
-        setCartBooks(
-            cartBooks.map((book) => {
-                if (book.id === bookToAdd.id) {
-                    return {
-                        ...book,
-
-                        quantity: book.quantity < 10 ? book.quantity + 1 : book.quantity,
-                    };
-                }
-                return book;
-            })
-        );
-    };
-
-    const removeQuantity = (bookToRemove: Book) => {
-        setCartBooks(
-            cartBooks.map((book) => {
-                if (book.id === bookToRemove.id) {
-                    return {
-                        ...book,
-                        quantity: book.quantity > 1 ? book.quantity - 1 : 1,
-                    };
-                }
-                return book;
-            })
-        );
-    };
+    const { cartBooks } = useContext(CartContext);
 
     return (
         <>
@@ -47,50 +14,7 @@ export default function CartPage() {
 
             <ul className="cart-list__items">
                 {cartBooks.map((book) => {
-                    return (
-                        <li className="cart-list__item">
-                            <div className="cart-list__image-wrapper">
-                                <img src={book.cover} alt={book.title} className="cart-list__image" />
-                            </div>
-                            <div className="cart-list__details-wrapper">
-                                <div className="cart-list__name-wrapper">
-                                    <h3 className="cart-list__name">{book.title}</h3>
-
-                                    <span className="cart-list__quantity">Quantity</span>
-                                    <button className="cart-list__button">
-                                        <button
-                                            className="cart-list__button-count"
-                                            onClick={() => {
-                                                removeQuantity(book);
-                                            }}
-                                        >
-                                            -
-                                        </button>{' '}
-                                        {book.quantity}{' '}
-                                        <button
-                                            className="cart-list__button-count"
-                                            onClick={() => {
-                                                addQuantity(book);
-                                            }}
-                                        >
-                                            +
-                                        </button>
-                                    </button>
-                                </div>
-                                <div className="cart-list__price-wrapper">
-                                    <p className="cart-list__price">{book.price} kr SEK</p>
-                                    <button
-                                        className="cart-list__remove-button"
-                                        onClick={() => {
-                                            removeFromCart(book);
-                                        }}
-                                    >
-                                        remove
-                                    </button>
-                                </div>
-                            </div>
-                        </li>
-                    );
+                    return <CartBookItem book={book} key={book.id} />;
                 })}
             </ul>
         </>
